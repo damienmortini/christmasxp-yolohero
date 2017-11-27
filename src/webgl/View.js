71f62da55fa8ca2d1758778a131f82ee253b7371
Damien Mortini <ddotmnn+git@gmail.com>
2017-11-27 22:40:00 +1100

    add text

--- src/webgl/View.js
+++ src/webgl/View.js
@@ -1,9 +1,13 @@
 import Matrix4 from "dlib/math/Matrix4.js";
 import GLProgram from "dlib/gl/GLProgram.js";
 import GLMesh from "dlib/gl/GLMesh.js";
+import GLText from "dlib/gl/GLText.js";
 import Camera from "dlib/3d/Camera.js";
 import TrackballController from "dlib/3d/controllers/TrackballController.js";
 import Background from "./Background.js";
+import Loader from "dlib/utils/Loader.js";
+
+Loader.load("src/Shrikhand-Regular.ttf");
 
 export default class View {
   constructor({
@@ -17,12 +21,22 @@ export default class View {
 
     this.cameraController = new TrackballController({
       matrix: this.camera.transform,
-      distance: 5
+      distance: 10
     });
 
     this.background = new Background({
       gl: this.gl,
       webcam: this.webcam
+    });
+
+    this.text = new GLText({
+      gl: this.gl,
+      scale: .5,
+      textAlign: "center",
+      textContent: "Move",
+      fillStyle: "white",
+      font: "100px Shrikhand-Regular",
+      paddingX: 4
     });
   }
 
@@ -33,13 +47,19 @@ export default class View {
  
   update() {
     this.gl.clearColor(0, 0, 0, 1);
-    // this.gl.disable(this.gl.CULL_FACE);
-    // this.gl.enable(this.gl.DEPTH_TEST);
+    this.gl.enable(this.gl.CULL_FACE);
 
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
 
-    // this.cameraController.update();
+    this.cameraController.update();
     
     this.background.draw();
+
+    this.gl.enable(this.gl.BLEND);
+    this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
+    this.text.draw({
+      camera: this.camera
+    });
+    this.gl.disable(this.gl.BLEND);
   }
 }
